9f33779fc211436c734205e49ddb4762e40df17c
saramaia <contato.saramaia@gmail.com>
2021-03-28 10:52:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
-var filmes = ["Rei Leão", "Coco", "Capitão América"]
-
-var listaFilmes = ["https://m.media-amazon.com/images/M/MV5BYTYxNGMyZTYtMjE3MS00MzNjLWFjNmYtMDk3N2FmM2JiM2M1XkEyXkFqcGdeQXVyNjY5NDU4NzI@._V1_UX182_CR0,0,182,268_AL_.jpg", "https://m.media-amazon.com/images/M/MV5BYjQ5NjM0Y2YtNjZkNC00ZDhkLWJjMWItN2QyNzFkMDE3ZjAxXkEyXkFqcGdeQXVyODIxMzk5NjA@._V1_UY268_CR3,0,182,268_AL_.jpg", "https://m.media-amazon.com/images/M/MV5BMTYzOTc2NzU3N15BMl5BanBnXkFtZTcwNjY3MDE3NQ@@._V1_UX182_CR0,0,182,268_AL_.jpg"]
-
-//console.log(filmes[0])
-//filmes.push("Rei Leão")
-
-//console.log(filmes.length);
-//console.log(filmes[0])
-
-for(var i = 0; i < listaFilmes.length; i++){
-  document.write("<img src=" + listaFilmes[i] + ">")
-  document.write(filmes[i])
+function adicionarFilme(){
+  var campoFilmeFavorito = document.querySelector('#filme')
+ // console.log(campoFilmeFavorito)
+  var filmeFavorito = campoFilmeFavorito.value
   
+  validaFilme(filmeFavorito)
   
-  
-  //console.log(filmes[i])
+/*
+    if(filmeFavorito.endsWith('.jpg')){
+
+      listaFilmesTela(filmeFavorito)
+
+    } else{
+      alert("Insira uma imagem válida")
+    } */
+
+}
+
+function validaFilme(filme){
+  if (filme.endsWith('.jpg')){
+    listaFilmesTela(filme)
+  }
+  else {
+    alert("Insira uma imagem válida")
+  }
+
+}
+
+function listaFilmesTela(filme){
+  var listaFilmes = document.querySelector("#listaFilmes")
+  var testeFilme = listaFilmes.value
+  console.log(filme) 
+ 
+  var elementoFilme = "<img src="+ filme +">"
+  listaFilmes.innerHTML = listaFilmes.innerHTML + elementoFilme
   
 }
